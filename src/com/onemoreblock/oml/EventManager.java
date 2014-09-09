6e4afb7d0d2cd4abde1e9634706ff2a2ee99185f
Eugene Kramskoi <epowerj@gmail.com>
2014-09-08 22:51:38 -0400

    Stopped hunger change

--- src/com/onemoreblock/oml/EventManager.java
+++ src/com/onemoreblock/oml/EventManager.java
@@ -1,10 +1,21 @@
 package com.onemoreblock.oml;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 
 /**
  * Created by epowerj on 9/8/14.
  */
 public class EventManager implements Listener {
 
+    @EventHandler
+    public void stopHungerChange(FoodLevelChangeEvent event) {
+        if ((event.getEntity() instanceof Player)) {
+            Player player = (Player) event.getEntity();
+            event.setCancelled(true);
+            player.setFoodLevel(6);
+        }
+    }
 }
