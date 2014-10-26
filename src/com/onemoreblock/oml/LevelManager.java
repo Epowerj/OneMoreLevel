450af5ef9ba4a605754b960ff829849dca82e418
Epowerj <epowerj@gmail.com>
2014-10-26 13:48:28 -0400

    Remade clone()

--- src/com/onemoreblock/oml/LevelManager.java
+++ src/com/onemoreblock/oml/LevelManager.java
@@ -3,7 +3,9 @@ package com.onemoreblock.oml;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by epowerj on 9/8/14.
@@ -34,15 +36,38 @@ public class LevelManager {
 
     public World clone(World world, Player player) {
         String worldname = (world.getName() + "@" + player.getName());
-        return copyWorld(world, worldname);
+        copyWorld(world.getWorldFolder(), createWorld(worldname).getWorldFolder());
+        return getWorld(worldname);
     }
 
-    private World copyWorld(World source, String copyWorld) {
-        WorldCreator wc = new WorldCreator(copyWorld);
-        wc.copy(source);
-        return wc.createWorld();
+    public void copyWorld(File source, File target) {
+        try {
+            ArrayList<String> ignore = new ArrayList<String>(Arrays.asList("uid.dat", "session.dat"));
+            if (!ignore.contains(source.getName())) {
+                if (source.isDirectory()) {
+                    if (!target.exists())
+                        target.mkdirs();
+                    String files[] = source.list();
+                    for (String file : files) {
+                        File srcFile = new File(source, file);
+                        File destFile = new File(target, file);
+                        copyWorld(srcFile, destFile);
+                    }
+                } else {
+                    InputStream in = new FileInputStream(source);
+                    OutputStream out = new FileOutputStream(target);
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = in.read(buffer)) > 0)
+                        out.write(buffer, 0, length);
+                    in.close();
+                    out.close();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 
     public World getWorld(String name) {
         World world = Bukkit.getWorld(name);
